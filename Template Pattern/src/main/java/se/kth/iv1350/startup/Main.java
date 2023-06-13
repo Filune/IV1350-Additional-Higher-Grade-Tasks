bd331410bf351ff95f49720df4e33ccd4641bb4d
S. L <3577394+Filune@users.noreply.github.com>
2023-06-13 10:41:13 +0200

    Moved ObserverTemplate init to Main from View.

--- Template Pattern/src/main/java/se/kth/iv1350/startup/Main.java
+++ Template Pattern/src/main/java/se/kth/iv1350/startup/Main.java
@@ -3,6 +3,9 @@ package se.kth.iv1350.startup;
 import se.kth.iv1350.controller.Controller;
 import se.kth.iv1350.integration.LedgerSystem;
 import se.kth.iv1350.integration.Printer;
+import se.kth.iv1350.integration.TotalRevenueFileOutput;
+import se.kth.iv1350.model.ObserverTemplate;
+import se.kth.iv1350.view.TotalRevenueView;
 import se.kth.iv1350.view.View;
 
 /**
@@ -14,9 +17,15 @@ public class Main {
      * Starts application and executes sample code.
      */
     public static void main(String[] args) {
+
+        ObserverTemplate totalRevenueView = new TotalRevenueView();
+        ObserverTemplate totalRevenueFileOutput = new TotalRevenueFileOutput();
+
         Printer printer = new Printer();
         LedgerSystem ledger = new LedgerSystem();
         Controller contr = new Controller(printer, ledger);
+        contr.addObserver(totalRevenueView);
+        contr.addObserver(totalRevenueFileOutput);
         View view = new View(contr);
         view.sample();
     }
